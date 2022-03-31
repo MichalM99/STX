c98af0f06b133238740a1c2809716348d17d380b
Michał <154914@student.uwm.edu.pl>
2022-03-31 22:13:57 +0200

    Heroku

--- BookList/settings.py
+++ BookList/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_vb8is77$h8@(s#-*c*&5mo-_1m@y!wq4fn(h*+6=h!9w%gm$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  'recrutationbooklist.herokuapp.com',
