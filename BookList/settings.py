96c0042cf2e2b1219a851b19bc417a275d03ad61
Michał <154914@student.uwm.edu.pl>
2022-03-31 22:31:31 +0200

    Heroku

--- BookList/settings.py
+++ BookList/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'BookList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'BookList',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3307',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '35381914_pracadyplomowa',
-        'USER': '35381914_pracadyplomowa',
-        'PASSWORD': 'Admin123.',
-        'HOST': 'serwer2195960.home.pl',
-        'PORT': '3306',
+        'NAME': 'BookList',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '35381914_pracadyplomowa',
+#         'USER': '35381914_pracadyplomowa',
+#         'PASSWORD': 'Admin123.',
+#         'HOST': 'serwer2195960.home.pl',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
